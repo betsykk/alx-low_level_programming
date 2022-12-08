19db26b25fcfb3889d0c7275efb8ad6bf8bbb839
betsykk <betsyykebe@gmail.com>
2022-12-08 15:01:47 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 
 /**
-* main - Entry Point
-*
-* Return: Always 0 (Sucess)
-*/
-
+ * main - Entry Point
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {  
        	puts("\"Programming is like building a multilingual puzzle");
-	         return (0);
+        return (0);
 }
