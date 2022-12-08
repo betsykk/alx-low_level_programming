280a79098721a03649d4c105487bf5be504e1092
betsykk <betsyykebe@gmail.com>
2022-12-08 14:16:42 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Sucess)
- */
+* main - Entry point
+* Return: Always 0 (Sucess)
+*/
 
- int main(void)
- {
+int main(void)
+{
 	 puts("\"Programming is like building a multilingual puzzle") ;
 	         return (0);
- }
+}
