f96d771f7ca9bfb854a9ada117a235ff80188893
betsykk <betsyykebe@gmail.com>
2022-12-20 16:10:34 -0800

    pointers 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**main - Generates random valid passwords for the
- * 	  program 101-crackme.
+/**
+ * main - Generates random valid passwords for the
+ * program 101-crackme.
  *
  * Return: Always 0.
  */
